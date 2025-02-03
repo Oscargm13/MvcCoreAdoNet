3ae1032122468868e834982d545bd21433f18a13
Oscargm13 <oskitargm5@gmail.com>
2025-02-03 19:57:58 +0100

    Add project files.

---
+++ MvcCoreAdoNet/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreEmptyPractica.AppHost.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        RepositoryHospital repo = new RepositoryHospital();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            this.repo.DeleteHospital(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCoreAdoNet/Controllers/HospitalesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreAdoNet.Models;
+
+namespace MvcCoreAdoNet.Controllers
+{
+
+    public class HospitalesController : Controller
+    {
+        private RepositoryHospital repo;
+
+        public HospitalesController()
+        {
+            this.repo = new RepositoryHospital();
+        }
+
+        public IActionResult Index()
+        {
+            List<Hospital> hospitales = this.repo.GetHospitales();
+            return View(hospitales);
+        }
+        public IActionResult Details(int id)
+        {
+            Hospital hospital = this.repo.FindHospital(id);
+            return View(hospital);
+        }
+        public IActionResult Create(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Hospital hospital)
+        {
+            this.repo.CreateHospital(hospital.IdHospital, hospital.Nombre, hospital.Direccion, hospital.Telefono, hospital.Camas);
+            ViewData["MENSAJE"] = "Hospital insertado";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int id)
+        {
+            Hospital hospital = this.repo.FindHospital(id);
+            return View(hospital);
+        }
+        [HttpPost]
+        public IActionResult Update(Hospital hospital)
+        {
+            this.repo.UpdateHospital(hospital.IdHospital, hospital.Nombre, hospital.Direccion, hospital.Telefono, hospital.Camas);
+            ViewData["MENSAJE"] = "HOSPITAL MODIFICADO";
+            return View(hospital);
+        }
+        public IActionResult Delete(int id)
+        {
+            this.repo.DeleteHospital(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
